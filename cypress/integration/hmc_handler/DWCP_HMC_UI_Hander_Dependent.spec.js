1e9c13e58034a27f2055bd4febdee2ba60425ddf
Steve Chapman <stevey.cee@gmail.com>
2021-08-18 11:59:19 +0100

    Update DWCP_HMC_UI_Hander_Dependent.spec.js

--- cypress/integration/hmc_handler/DWCP_HMC_UI_Hander_Dependent.spec.js
+++ cypress/integration/hmc_handler/DWCP_HMC_UI_Hander_Dependent.spec.js
@@ -1,38 +1,36 @@
 /// <reference types="cypress" />
-beforeEach(() => {
-  // Setting initial state
-  cy.visit("https://ao.com/dishwashers");
+Cypress.Commands.add("totalProducts", () => {
+  cy.get("#helpMeChooseContainer")
+    .find("#shopNow")
+    .invoke("attr", "data-total-products");
 });
 
-describe("Dishwasher HMC Type Test", () => {
-  it(`should...`, () => {
-    cy.get("#helpMeChooseContainer")
-      .find("#shopNow")
-      .invoke("attr", "data-total-products")
-      .as("initialValue");
-    // Press the button
+before(() => {
+  cy.visit("https://ao.com/dishwashers");
+  // Used to wait for Post from the helpmechoose handler before comparing any values
+  cy.intercept("POST", "/handler/helpmechoose/query").as("hmcQuery");
+});
+
+describe("HMC (Dishwasher Type) Test", () => {
+  it(`should select an option on the HMC, check the number of products decreases, press the CTA and resolve to a lister`, () => {
+    // Alias inital value
+    cy.totalProducts().as("initialValue");
+
+    // Press first option
     cy.get("#helpMeChooseContainer")
       .find(".primary.facets")
       .find("li")
       .first()
       .click();
-    //
-    cy.get("#helpMeChooseContainer")
-      .find("#shopNow")
-      .invoke("attr", "data-total-products")
-      .as("firstClickValue");
-    cy.get("@initialValue").should("not.equal", "@firstClickValue");
-    // Set up 2nd interception
-    cy.get("#helpMeChooseContainer")
-      .find(".secondary.facets")
-      .find("li")
-      .first()
-      .click();
-    // Validate value of 2nd interception is being displayed
-    cy.get("#helpMeChooseContainer")
-      .find("#shopNow")
-      .invoke("attr", "data-total-products")
-      .as("secondClickValue");
+
+    // Instead of using a static time we are awaiting a response from the HMC handler as aliased in the before above
+    cy.wait("@hmcQuery");
+
+    // Check number decreases
+    cy.totalProducts().then(($newValue) => {
+      cy.get("@initialValue").should("not.equal", $newValue);
+    });
+
     // Validate the button goes to a lister
     cy.get("#helpMeChooseContainer").find("#shopNow").click();
     cy.url().should("contain", "/l/");
