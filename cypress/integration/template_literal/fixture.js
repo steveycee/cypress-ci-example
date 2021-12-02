dd20b12d58461d3ba67a7b96d66250b01e5c188a
Steve Chapman <stevey.cee@gmail.com>
2021-12-02 11:28:13 +0000

    Update fixture.js

--- cypress/integration/template_literal/fixture.js
+++ cypress/integration/template_literal/fixture.js
@@ -1,4 +1,4 @@
-// let testid = "header_logo_main";
+// I'm a fixture file which we can assume that as a tester I don't have access to edit, it contains a broken test id that I want to reference in one of my tests. Go to utility.js to see how we fix the id
 let brokenTestid = "headerlogo_main";
 
 export { brokenTestid };
