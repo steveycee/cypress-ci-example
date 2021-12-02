090649ed5beddcd71e0c52e96c8519e41040f19c
Steve Chapman <stevey.cee@gmail.com>
2021-12-02 11:27:19 +0000

    Update utility.js

--- cypress/integration/template_literal/utility.js
+++ cypress/integration/template_literal/utility.js
@@ -1,3 +1,5 @@
+// I'm a utility/helper function that will add a character to the broken test id in the fixture (i know we can fix it in the fixture but lets assume that we can't.
+
 import { brokenTestid } from "./fixture.js";
 
 let originalString = brokenTestid;
